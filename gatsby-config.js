d7145f76f8ec780dab8464d5a6fdd7c3a5c5b44a
gaoxu <gaoxu@laosiji.com>
2020-09-11 17:10:21 +0800

    ga统计

--- gatsby-config.js
+++ gatsby-config.js
@@ -108,9 +108,16 @@ module.exports = {
       },
     },
     `gatsby-plugin-glamor`,
-    `gatsby-plugin-sass`
+    `gatsby-plugin-sass`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
+        trackingId: "UA-101757170-1",
+      },
+    },
   ],
 }
